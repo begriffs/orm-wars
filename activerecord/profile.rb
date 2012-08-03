c39081f0660918f0ac6602598f22b59c938a061a
Joe Nelson <cred+github@begriffs.com>
2012-08-03 09:56:02 -0500

    activerecord pg connection established

---
+++ activerecord/profile.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'active_record'
+require 'logger'
+require 'yaml'
+
+ActiveRecord::Base.establish_connection(
+  YAML::load(File.open('db/config.yml'))
+)
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class User < ActiveRecord::Base
+end
+ 
+puts User.count
