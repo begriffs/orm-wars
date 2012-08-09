b250b8858c9f032ae0929e2b87583717730251c3
Joe Nelson <cred+github@begriffs.com>
2012-08-09 18:24:02 -0500

    basic sqlalchemy test

---
+++ sqlalchemy/test.py
@@ -0,0 +1,13 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+engine = create_engine('postgresql://localhost/sqlalchemy_test', echo=True)
+Base = declarative_base(bind=engine)
+Session = scoped_session(sessionmaker(engine))
+
+class User(Base):
+  __tablename__ = 'users'
+  id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+
+Base.metadata.create_all()
