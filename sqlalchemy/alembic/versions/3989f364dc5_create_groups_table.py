24c67be5a5f7a7fdd2d06f558631c1866a70bd69
Joe Nelson <cred+github@begriffs.com>
2012-08-09 22:11:59 -0500

    adds groups table

---
+++ sqlalchemy/alembic/versions/3989f364dc5_create_groups_table.py
@@ -0,0 +1,26 @@
+"""create groups table
+
+Revision ID: 3989f364dc5
+Revises: 39e93e7ef50b
+Create Date: 2012-08-09 21:51:30.333261
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '3989f364dc5'
+down_revision = '39e93e7ef50b'
+
+from alembic import op
+import sqlalchemy as sa
+
+def upgrade():
+  op.create_table(
+    'groups',
+    sa.Column('id', sa.Integer, sa.Sequence('group_id_seq'), primary_key=True)
+  )
+  op.add_column('users',
+    sa.Column('group_id', sa.Integer, sa.ForeignKey('groups.id')))
+
+def downgrade():
+  op.drop_column('users', 'group_id')
+  op.drop_table('groups')
--- sqlalchemy/test.py
+++ sqlalchemy/test.py
@@ -6,8 +6,13 @@ engine = create_engine('postgresql://localhost/sqlalchemy_test', echo=True)
 Base = declarative_base(bind=engine)
 Session = scoped_session(sessionmaker(engine))
 
+class Group(Base):
+  __tablename__ = 'groups'
+  id = Column(Integer, Sequence('group_id_seq'), primary_key=True)
+
 class User(Base):
   __tablename__ = 'users'
   id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+  group_id = Column(Integer, ForeignKey('groups.id'))
 
 print Session.query(User).count()
